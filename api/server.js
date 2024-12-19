36feded8ac4c3eb6859916b3073a77545fdd9718
Isabel <isabel.dapper@agrocpr.com>
2024-12-18 23:49:02 -0300

    try to emualate a valid request

--- api/server.js
+++ api/server.js
@@ -33,12 +33,12 @@ const Bio = mongoose.model('Bio', bioSchema);
 
 async function getInstagramProfileMeta(url) {
   try {
-    const { data } = await axios.get(url, { headers: { 'User-Agent': 'Mozilla/5.0' } });
-    console.log('data', data);
-
+    const { data } = await axios.get(url, { headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      'Origin': 'https://www.instagram.com', 
+      'Referer': 'https://www.instagram.com/',
+    }});
     const $ = cheerio.load(data);
-    console.log('metadata', $("meta[property='og:image']"));
-
     const profileImage = $("meta[property='og:image']").attr('content');
     console.log('profileImage', profileImage);
     
